de5d87616294db5039ba93247447209e921e0abc
montiho8 <montiho8@hawaii.edu>
2022-10-05 21:22:30 -1000

    Completed Parts 2-8
    Swift Playgrounds #3

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -1,3 +1,30 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+//Part 2: How to create a variable that is an array? Google search results: To create an array, define the data type (like int) and specify the name of the array followed by square brackets [].
+
+//Part 3
+var fruitNames: [String]
+//var numbers: [Int]
+
+//Part 4
+fruitNames = ["Kiwi", "Apples", "Watermelon"]
+
+//Part 5
+print(fruitNames)
+
+//Part 6
+print("I like to eat \(fruitNames)")
+
+//Part 7: Iterate through all items in the array using the "for-in" syntax
+for name in fruitNames{
+    print("I like to eat " + name)
+}
+
+//Part 8: Introduction to array indexes
+print(fruitNames[1])
+
+
+//Part 9
+
